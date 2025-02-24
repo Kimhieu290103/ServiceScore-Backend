30f6aa3c4d8adc3d221879e87c243b24bc8396fa
Oreki1001 <trankimhieu1001@gmail.com>
2025-02-24 19:31:12 +0700

    Initial commit

---
+++ src/test/java/dtn/ServiceScore/ServiceScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dtn.ServiceScore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
